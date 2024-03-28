1279e70099b1710024938145290d0d4f2ac943fd
NadilaAmaliaaa <nadilaamaliapribadi@gmail.com>
2024-03-28 12:24:18 +0700

    Migration 1

---
+++ database/migrations/2024_03_28_042641_create_user_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->id('id_user');
+            $table->string('nama', 250);
+            $table->string('password', 8);
+            $table->boolean('isAdmin');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user');
+    }
+};
---
+++ database/migrations/2024_03_28_050508_create_berita_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('berita', function (Blueprint $table) {
+            $table->id('id_berita');
+            $table->unsignedBigInteger('id_admin')->index();
+            $table->string('url_gambar', 255);
+            $table->string('judul', 100);
+            $table->text('isi');
+            $table->timestamps();
+
+            $table->foreign('id_admin')->references('id_admin')->on('admin');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('berita');
+    }
+};
