3ccf092b83e473a417395d3635db09a47860b34d
Zalbaq <ihza.nk.160903@gmail.com>
2024-04-04 13:48:44 +0700

    [add] constants variable

---
+++ config/constants.php
@@ -0,0 +1,33 @@
+<?php
+return [
+    [
+        "imgURL" => 'assets/icons/statistik.svg',
+        "route" => 'statistik',
+        "label" => "Statistik",
+    ],
+    [
+        "imgURL" => 'assets/icons/data-dasawisma.svg',
+        "route" => 'data-dasawisma',
+        "label" => "Data Dasawisma",
+    ],
+    [
+        "imgURL" => 'assets/icons/manajemen-dokumen.svg',
+        "route" => 'manajemen-dokumen',
+        "label" => "Manajemen Dokumen",
+    ],
+    [
+        "imgURL" => 'assets/icons/manajemen-dana.svg',
+        "route" => 'manajemen-dana',
+        "label" => "Manajemen Dana",
+    ],
+    [
+        "imgURL" => 'assets/icons/manajemen-berita.svg',
+        "route" => 'manajemen-berita',
+        "label" => "Manajemen berita",
+    ],
+    [
+        "imgURL" => 'assets/icons/edit-profil.svg',
+        "route" => 'edit-profil',
+        "label" => "Ubah Profil",
+    ]
+];
