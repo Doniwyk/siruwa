44cad26ef30575bae68a55479e4ccbdec5fb041d
Zalbaq <ihza.nk.160903@gmail.com>
2024-04-04 20:42:08 +0700

    [add] routing for admin

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\BeritaController;
+use App\Http\Controllers\DanaController;
+use App\Http\Controllers\DataDasawismaController;
+use App\Http\Controllers\DokumenController;
+use App\Http\Controllers\ProfilController;
+use App\Http\Controllers\StatistikController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,9 +20,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('landingpage');
 });
 
-Route::get('/', function () {
-    return view('landingpage');
+Route::prefix('admin')->group(function () {
+    Route::get('/', [StatistikController::class, 'index'])->name('statistik');
+    Route::get('/data-dasawisma', [DataDasawismaController::class, 'index'])->name('data-dasawisma');
+    Route::get('/manajemen-dokumen', [DokumenController::class,'index'])->name('manajemen-dokumen');
+    Route::get('/manajemen-dana', [DanaController::class,'index'])->name('manajemen-dana');
+    Route::get('/manajemen-berita', [BeritaController::class,'index'])->name('manajemen-berita');
+    Route::get('/edit-profil', [ProfilController::class,'index'])->name('edit-profil');
 });
