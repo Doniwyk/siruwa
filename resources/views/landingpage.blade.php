d14f7be9ea9b399c7beef38e6fcb50233a4f7207
Zalbaq <ihza.nk.160903@gmail.com>
2024-04-04 13:50:28 +0700

    [rename] class to newest class tailwind

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -1,29 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SiUwU</title>
     @vite('resources/css/app.css')
 </head>
-<body class="">
-    <div class="z-10 fixed bg-nav w-full px-20 py-4 items-center gap-6 inline-flex justify-end padding margin-10">
-        <span class="text-center text-stone-50 text-3xl font-extrabold font-['Baloo 2']">>///<</span>
-        <div class="w-screen h-14 text-center gap-8 flex justify-end items-center">
-            <span class="text-white text-base font-medium font-['Anek Tamil']">KEPENGURUSAN</span>
-            <span class="text-white text-base font-medium font-['Anek Tamil']">AGENDA</span>
-            <span class="text-white text-base font-medium font-['Anek Tamil']">BERITA</span>
-            <span class="text-white text-base font-medium font-['Anek Tamil']">MENU</span>
-            <span class="text-white text-base font-medium font-['Anek Tamil']">BERANDA</span>
-        </div>
 
-        <!-- MyUwU login -->
-        <div class="bg-white rounded-2xl justify-center items-center gap-8 flex-auto">
-            <div class="justify-center items-center gap-4 inline-flex py-2 px-8">
-                <span class="text-center text-teal-900 text-sm font-medium font-['Anek Tamil']">MyUwU</span>
-                <img class="w-11 h-11 rounded-full" src="https://via.placeholder.com/44x44" />
-            </div>
-        </div>
+<body class="">
+    <div class="z-10 fixed bg-main w-full px-20 py-4 items-center gap-6 inline-flex justify-end padding margin-10">
+        <span class="text-center text-stone-50 text-3xl font-extrabold font-['Baloo 2']">>///<< /span>
+                <div class="w-screen h-14 text-center gap-8 flex justify-end items-center">
+                    <span class="text-white text-base font-medium font-['Anek Tamil']">KEPENGURUSAN</span>
+                    <span class="text-white text-base font-medium font-['Anek Tamil']">AGENDA</span>
+                    <span class="text-white text-base font-medium font-['Anek Tamil']">BERITA</span>
+                    <span class="text-white text-base font-medium font-['Anek Tamil']">MENU</span>
+                    <span class="text-white text-base font-medium font-['Anek Tamil']">BERANDA</span>
+                </div>
+
+                <!-- MyUwU login -->
+                <div class="bg-white rounded-2xl justify-center items-center gap-8 flex-auto">
+                    <div class="justify-center items-center gap-4 inline-flex py-2 px-8">
+                        <span class="text-center text-teal-900 text-sm font-medium font-['Anek Tamil']">MyUwU</span>
+                        <img class="w-11 h-11 rounded-full" src="https://via.placeholder.com/44x44" />
+                    </div>
+                </div>
     </div>
 
     <!-- Beranda -->
@@ -31,14 +33,17 @@
         <div class="flex flex-col justify-start items-center gap-16 w-3/4">
             <div class="flex flex-col gap-10">
                 <span class="text-center text-white text-8xl font-extrabold font-['Anek Tamil'] leading-10">SIUwU</span>
-                <span class="text-center text-white text-4xl font-medium font-['Anek Tamil'] leading-10">Sistem Informasi Kawaiiiii</span>
+                <span class="text-center text-white text-4xl font-medium font-['Anek Tamil'] leading-10">Sistem
+                    Informasi Kawaiiiii</span>
             </div>
-            <p class="text-center text-white text-xl font-medium font-['Anek Tamil'] leading-9">Selamat datang di SIUwU, jelajahi aplikasi web mutakhir untuk mengatur segala hal di dunia.</p>
+            <p class="text-center text-white text-xl font-medium font-['Anek Tamil'] leading-9">Selamat datang di SIUwU,
+                jelajahi aplikasi web mutakhir untuk mengatur segala hal di dunia.</p>
         </div>
     </section>
 
     <section class="bg_beranda opacity-60 flex justify-center content-center p-72 w-full h-screen">
-        
+
     </section>
 </body>
+
 </html>
