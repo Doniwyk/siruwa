b10974741c9cab90ca229637d41ad7c9ef35b173
Zalbaq <ihza.nk.160903@gmail.com>
2024-05-01 21:34:07 +0700

    [set] routing for admin

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
+use App\Http\Controllers\AccountController;
 use App\Http\Controllers\BeritaController;
 use App\Http\Controllers\DanaController;
 use App\Http\Controllers\DataDasawismaController;
+use App\Http\Controllers\DocumentController;
 use App\Http\Controllers\DokumenController;
+use App\Http\Controllers\NewsController;
 use App\Http\Controllers\ProfilController;
 use App\Http\Controllers\StatistikController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,10 +28,10 @@ Route::get('/', function () {
 });
 
 Route::prefix('admin')->group(function () {
-    Route::get('/', [StatistikController::class, 'index'])->name('statistik');
-    Route::get('/data-dasawisma', [DataDasawismaController::class, 'index'])->name('data-dasawisma');
-    Route::get('/manajemen-dokumen', [DokumenController::class,'index'])->name('manajemen-dokumen');
-    Route::get('/manajemen-dana', [DanaController::class,'index'])->name('manajemen-dana');
-    Route::get('/manajemen-berita', [BeritaController::class,'index'])->name('manajemen-berita');
-    Route::get('/edit-profil', [ProfilController::class,'index'])->name('edit-profil');
+    // Route::get('/', [StatistikController::class, 'index'])->name('statistik');
+    Route::get('/data-dasawisma', [UserController::class, 'index'])->name('data-dasawisma');
+    Route::get('/manajemen-dokumen', [DocumentController::class,'index'])->name('manajemen-dokumen');
+    // Route::get('/manajemen-dana', [DanaController::class,'index'])->name('manajemen-dana');
+    Route::get('/manajemen-berita', [NewsController::class,'index'])->name('manajemen-berita');
+    Route::get('/edit-profil', [AccountController::class,'index'])->name('edit-profil');
 });
