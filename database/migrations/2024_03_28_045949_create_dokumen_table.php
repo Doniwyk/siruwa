aeef55ad46a85ca32a5f50caaaabb333654ef29c
NadilaAmaliaaa <nadilaamaliapribadi@gmail.com>
2024-05-06 15:52:59 +0700

    [fix] db migration and model

--- database/migrations/2024_03_28_045949_create_dokumen_table.php
+++ database/migrations/2024_03_28_045949_create_dokumen_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id('id_dokumen');
             $table->unsignedBigInteger('id_penduduk')->index();
             $table->string('jenis', 100);
-            $table->enum('status', ['Bisa Diambil', 'Proses Verifikasi', 'Ditolak'])->default('Proses Verifikasi');
+            $table->enum('status', ['Bisa Diambil', 'Menunggu Verifikasi', 'Dalam Proses', 'Ditolak'])->default('Menunggu Verifikasi');
             $table->timestamps();
 
             $table->foreign('id_penduduk')->references('id_penduduk')->on('penduduk');
