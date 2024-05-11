b3cef67331dd5f03bc478a59a300dbdb6bd47a3a
Zalbaq <ihza.nk.160903@gmail.com>
2024-05-11 22:36:12 +0700

    [edit] topbar

--- resources/views/components/shared/topbar.blade.php
+++ resources/views/components/shared/topbar.blade.php
@@ -1,3 +1,8 @@
 <div class="topbar">
-    asuu
+    <div class="main-logo flex-center ">
+        <div class="text-white text-2xl font-extrabold">
+            <label for="" >Siruwa</label>
+        </div>
+    </div>
+    <x-topbar-profile />
 </div>
