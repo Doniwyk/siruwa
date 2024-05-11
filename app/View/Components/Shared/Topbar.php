1471ed2143b166d67855abbf002d71a51c80c8b8
Zalbaq <ihza.nk.160903@gmail.com>
2024-05-11 22:46:41 +0700

    [add] main topbar component

--- app/View/Components/Shared/Topbar.php
+++ app/View/Components/Shared/Topbar.php
@@ -21,6 +21,6 @@ class Topbar extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components..shared.topbar');
+        return view('components.shared.topbar');
     }
 }
