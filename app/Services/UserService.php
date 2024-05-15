cbc6cf37ac68fd3c34d5e1d936b3e3e9814e0e63
putricha_ <putrinorchasana@gmail.com>
2024-05-16 06:10:50 +0700

    [update] add function to user contract and implement on user service

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -5,7 +5,9 @@ namespace App\Services;
 use App\Contracts\PendudukContract;
 use App\Contracts\UserContract;
 use App\Models\PendudukModel;
+use App\Models\TempResidentModel;
 use App\Models\UserModel;
+use Illuminate\Http\Request;
 
 class UserService implements UserContract
 {
@@ -23,5 +25,37 @@ class UserService implements UserContract
     public function deleteUser(UserModel $penduduk):void
     {
         $penduduk->delete();
+    }
+
+    public function validateEditRequest(Request $request, UserModel $resident)
+    {
+        $tempResident = TempResidentModel::where('nik', $resident->nik)->first();
+        if ($request->action === 'accept') {
+            $resident->update((array) $tempResident);
+            $tempResident->status = 'Disetujui';
+            $tempResident->save();
+        }
+
+        if ($request->action === 'reject') {
+            $tempResident->status = 'Ditolak';
+            $tempResident->save();
+        }
+    }
+    
+    public function editRequest(Request $request , UserModel $resident)
+    {
+        $existingRequest = TempResidentModel::where('id_penduduk', $resident->id_penduduk)
+            ->where('status', 'Menunggu Verifikasi')
+            ->exists();
+        if ($existingRequest) {
+            return false;
+        } else {
+            $resident = UserModel::find($resident->id_penduduk);
+            $mergeResident = array_merge($resident->toArray(), $request->toArray());
+            TempResidentModel::create($mergeResident);
+            return true;
+        }
+
+
     }
 }
