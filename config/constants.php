51421c98a99a5d34f11e98b80876cacdcb1f75c3
Zalbaq <ihza.nk.160903@gmail.com>
2024-05-16 06:25:43 +0700

    change[route]: change route name to store more name of route with array

--- config/constants.php
+++ config/constants.php
@@ -2,32 +2,32 @@
 return [
     [
         "imgURL" => 'assets/icons/statistik.svg',
-        "route" => 'statistic',
+        "route" => ['statistic',''],
         "label" => "Statistik",
     ],
     [
         "imgURL" => 'assets/icons/data-dasawisma.svg',
-        "route" => 'data-penduduk',
+        "route" => ['data-penduduk', 'edit-data-penduduk'], 
         "label" => "Data Penduduk",
     ],
     [
         "imgURL" => 'assets/icons/manajemen-dokumen.svg',
-        "route" => 'manajemen-dokumen',
+        "route" => ['manajemen-dokumen',''],
         "label" => "Manajemen Dokumen",
     ],
     [
         "imgURL" => 'assets/icons/manajemen-dana.svg',
-        "route" => 'manajemen-dana',
+        "route" => ['manajemen-dana',''],
         "label" => "Manajemen Dana",
     ],
     [
         "imgURL" => 'assets/icons/manajemen-berita.svg',
-        "route" => 'manajemen-berita',
+        "route" => ['manajemen-berita',''],
         "label" => "Manajemen Berita",
     ],
     [
         "imgURL" => 'assets/icons/edit-profil.svg',
-        "route" => 'profil',
+        "route" => ['profil',''],
         "label" => "Ubah Profil",
     ]
 ];
