9d241f0c9536d353cda861e6b9fef7024a50762f
doniwyk <doniwyk@gmail.com>
2024-05-16 10:38:58 +0700

    [update] rename  to

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ $page }}</title>
+    <title>{{ $title }}</title>
     @vite('resources/css/app.css')
     @vite('resources/css/output.css')
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@2.8.2/dist/alpine.min.js" defer></script>
