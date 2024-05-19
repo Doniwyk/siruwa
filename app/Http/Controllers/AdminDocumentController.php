d4c8d741da956b60d79b1078963aa67a378741ac
Ihza Nurkhafidh Al-baihaqi <ihza.nk.160903@gmail.com>
2024-05-19 10:16:07 +0700

    connect [function]: connecting function validateDocument to validate page for admin

--- app/Http/Controllers/AdminDocumentController.php
+++ app/Http/Controllers/AdminDocumentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Contracts\AdminDocumentContract;
+use Illuminate\Http\Request;
 use App\Http\Requests\ValidateDocumentRequest;
 use App\Models\DocumentModel;
 
@@ -17,10 +18,14 @@ class AdminDocumentController extends Controller
         $this->documentService = $documentService;
     }
     
-    public function index()
+    public function index(Request $request)
     {
-        $documentData = $this->documentService->getDocumentRequest();
-        return view('admin._document.index', compact('documentData'));
+        $typeDocument = $request->query('typeDocument', 'pengajuan');
+        $page = 'data-dokumen';
+        $title = 'Manajemen Dokumen';
+        $documents = $this->documentService->getDocumentRequest();
+
+        return view('admin._document.index', compact('documents', 'typeDocument', 'page', 'title'));
     }
   
     public function validateDocument(ValidateDocumentRequest $request, string $action, DocumentModel $document)
@@ -38,9 +43,13 @@ class AdminDocumentController extends Controller
         $documentHistory = $this->documentService->getValidateHistory();
         return view('admin._document.history', compact('documentHistory'));
     }
-    public function getEditPage()
+    public function getEditPage(DocumentModel $document)
     {
-        return view('admin._document.edit');
+        $page = 'edit-data-dokumen';
+        $title = 'Manajemen Dokumen';
+        $documents = $this->documentService->getDocumentRequest();
+        $document = $document->findOrFail($document->id_dokumen);
+        return view('admin._document.edit', compact('page', 'title', 'document'));
     }
     public function changeStatus(ValidateDocumentRequest $request, DocumentModel $document){
         try {
