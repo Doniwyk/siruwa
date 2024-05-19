b722b687d7db7eee078ad0d84eaaf5fa9c6c7d9a
Ihza Nurkhafidh Al-baihaqi <ihza.nk.160903@gmail.com>
2024-05-19 10:06:03 +0700

    create [component]: create component to provide disabled form view from DB

---
+++ app/View/Components/form/inputForm_Show.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\View\Components\form;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class inputForm_Show extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public $label;
+    public $value;
+    public $name;
+    public function __construct($label, $value, $name)
+    {
+        $this->label = $label;
+        $this->value = $value;
+        $this->name = $name;
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.form.input-form_-show');
+    }
+}
