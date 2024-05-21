6cd3908acd2e5708585e1db54eac564f02f0453f
Ihza Nurkhafidh Al-baihaqi <ihza.nk.160903@gmail.com>
2024-05-22 06:05:15 +0700

    create [component]: create component for action on admin when cancel or acc the document

---
+++ resources/views/components/shared/action-description-form.blade.php
@@ -0,0 +1,20 @@
+<form id="{{ $formId }}" class=" absolute p-10 rounded-2xl flex-col bg-white hidden"
+    action="{{ route('admin.data-dokumen.changeStatus', ['document' => $document->id_dokumen]) }}" method="POST">
+    @csrf
+    @method('PUT')
+    <button class="absolute w-[25px] h-[25px] -right-2 -top-2 rounded-full bg-red-600" onclick="closeForm('#{{ $formId }}')"></button>
+    <h1 class="text-xl font-bold text-main">Catatan</h1>
+    <textarea name="keterangan_status" id="keterangan_status"
+        class="h-[10rem] w-[22rem] mt-4 border-2 p-2 border-outline outline-none rounded-2xl" required></textarea>
+    <button type="submit" @class([
+        'mt-5',
+        'bg-red-600' => $statusValue == 'Ditolak',
+        'bg-main' => $statusValue == 'Bisa Diambil',
+        'text-white',
+        'px-4',
+        'py-2',
+        'rounded-md',
+        'flex-1',
+    ]) type="submit" name="status"
+        value="{{ $statusValue }}">{{ $statusValue == 'Ditolak' ? 'Batalkan' : 'Konfirmasi' }}</button>
+</form>
