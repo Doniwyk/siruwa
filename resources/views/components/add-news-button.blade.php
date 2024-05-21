c8a3a090c5077d9c35b0f2cccf8bdaac58d865c0
Ihza Nurkhafidh Al-baihaqi <ihza.nk.160903@gmail.com>
2024-05-22 06:14:10 +0700

    create [component]: create component to create news

---
+++ resources/views/components/add-news-button.blade.php
@@ -0,0 +1,14 @@
+<div class="add-news flex justify-end">
+    <button class="h-[3rem] w-[15rem] bg-main text-white rounded-2xl flex-center gap-3">
+        <svg width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+                d="M10.4993 18.3332C15.0827 18.3332 18.8327 14.5832 18.8327 9.99984C18.8327 5.4165 15.0827 1.6665 10.4993 1.6665C5.91602 1.6665 2.16602 5.4165 2.16602 9.99984C2.16602 14.5832 5.91602 18.3332 10.4993 18.3332Z"
+                stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M7.16602 9.99902H13.8327" stroke="white" stroke-width="2" stroke-linecap="round"
+                stroke-linejoin="round" />
+            <path d="M10.5 13.3327V6.66602" stroke="white" stroke-width="2" stroke-linecap="round"
+                stroke-linejoin="round" />
+        </svg>
+        <label for="">Buat Berita</label>
+    </button>
+</div>
