e5b75011bd14d2950d2ebf74f1c6fb03b66c88d9
Ihza Nurkhafidh Al-baihaqi <ihza.nk.160903@gmail.com>
2024-05-23 22:56:56 +0700

    add [condition]: add condition to rendering the table based on type of data

--- resources/views/admin/_fund/index.blade.php
+++ resources/views/admin/_fund/index.blade.php
@@ -2,8 +2,8 @@
 @section('content')
     <h1 class="h1-semibold">Data Penduduk</h1>
     <div class="summary-card_fund">
-        <x-card :label="'Dana Kematian'" :value="9000000" />
-        <x-card :label="'Dana Sampah'" :value="9000000" />
+        <x-card :label="'Dana Kematian'" :value="$fundData['deathFundTotal']" />
+        <x-card :label="'Dana Sampah'" :value="$fundData['garbageFundTotal']" />
         <x-card :label="'Tunggakan'" :type="'danger'" :value="9000000" />
     </div>
     <div class="link-option_parrent">
@@ -31,26 +31,47 @@
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach ($fundData as $data)
+                    @php
+                        $i = 1;
+                    @endphp
+                    @foreach ($fundData['getSubmission'] as $data)
                         <tr>
                             <td>{{ $data->nomor_kk }}</td>
                             <td>{{ $data->created_at }}</td>
                             <td>{{ $data->admin->noHp }}</td>
                             <td>
-                                <button class="w-[25px] h-[25px] flex-center" onclick="showPopup('#payment-card')">
+                                <button class="w-[25px] h-[25px] flex-center"
+                                    onclick="showPopup('#payment-card-{{ $i }}', '#modal-parent-{{ $i }}')">
                                     <x-icon.detail />
                                 </button>
                             </td>
-                            <td class="w-full h-full flex-center bg-black/50 absolute top-0 left-0 hidden" id="modal-parent">
-                                <x-shared.confirmation-payment-card :formId="'payment-card'" :fundData="$data" :statusValue="$data->status" />
+                            <td class="w-full h-full flex-center bg-black/50 absolute top-0 left-0 hidden"
+                                id="modal-parent-{{ $i }}">
+                                <x-shared.confirmation-payment-card :formId="$i" :fundData="$data" />
                             </td>
                         </tr>
+                        @php
+                            $i++;
+                        @endphp
                     @endforeach
                 </tbody>
             </table>
         @break
 
         @case('riwayat')
+            <table class="table-parent">
+                <thead>
+                    <tr>
+                        <th>Nomor KK</th>
+                        <th>Tgl Permintaan</th>
+                        <th>No. Telepon</th>
+                        <th>Detail</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                </tbody>
+            </table>
         @break
 
         @default
