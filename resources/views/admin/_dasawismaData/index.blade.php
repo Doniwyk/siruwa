e282243edef5d5270ec6fe5bbfee8af0b7f1a85a
Ihza Nurkhafidh Al-baihaqi <ihza.nk.160903@gmail.com>
2024-05-24 17:56:54 +0700

    add [section]: add section to wrap add button with tab button

--- resources/views/admin/_dasawismaData/index.blade.php
+++ resources/views/admin/_dasawismaData/index.blade.php
@@ -1,27 +1,41 @@
 @extends('layouts.admin')
 @section('content')
     <h1 class="h1-semibold">Data Penduduk</h1>
-    <div class="link-option_parrent">
-        <a href="{{ route('admin.data-penduduk.index', ['typeDocument' => 'daftar-penduduk']) }}"
-            @class([
+    <section class="w-full flex-between">
+        <div class="link-option_parrent">
+            <a href="{{ route('admin.data-penduduk.index', ['typeDocument' => 'daftar-penduduk']) }}"
+                @class([
+                    'link-option',
+                    'link-option_active' => $typeDocument == 'daftar-penduduk',
+                ])>
+                Daftar Penduduk
+            </a>
+            <a href="{{ route('admin.data-penduduk.index', ['typeDocument' => 'pengajuan']) }}" @class([
                 'link-option',
-                'link-option_active' => $typeDocument == 'daftar-penduduk',
+                'link-option_active' => $typeDocument == 'pengajuan',
             ])>
-            Daftar Penduduk
-        </a>
-        <a href="{{ route('admin.data-penduduk.index', ['typeDocument' => 'pengajuan']) }}" @class([
-            'link-option',
-            'link-option_active' => $typeDocument == 'pengajuan',
-        ])>
-            Pengajuan
-        </a>
-        <a href="{{ route('admin.data-penduduk.index', ['typeDocument' => 'riwayat']) }}" @class([
-            'link-option',
-            'link-option_active' => $typeDocument == 'riwayat',
-        ])>
-            Riwayat
-        </a>
-    </div>
+                Pengajuan
+            </a>
+            <a href="{{ route('admin.data-penduduk.index', ['typeDocument' => 'riwayat']) }}" @class([
+                'link-option',
+                'link-option_active' => $typeDocument == 'riwayat',
+            ])>
+                Riwayat
+            </a>
+        </div>
+        <div class="add-wrapper">
+            <button class="flex-between gap-[10px] px-6 py-3 bg-white rounded-2xl font-semibold text-main">
+                <x-icon.import />
+                Import .csv
+            </button>
+            <a href="{{ route('admin.data-penduduk.add') }}">
+            <button class="flex-between gap-[10px] px-6 py-3 bg-main rounded-2xl font-semibold text-white">
+                <x-icon.add-circle />
+                Tambah Data
+            </button>
+            </a>
+        </div>
+    </section>
 
     <x-filter :typeDocument=$typeDocument :search="$search" :order="$order" />
 
@@ -82,27 +96,27 @@
                 </thead>
                 <tbody>
                     @foreach ($residents as $resident)
-                            <tr>
-                                <td class="hidden">{{ $resident->id_penduduk }}</td>
-                                <td>{{ $resident->penduduk->nama }}</td>
-                                <td>{{ $resident->penduduk->nik }}</td>
-                                <td>{{ $resident->penduduk->tgl_lahir }}</td>
-                                <td>{{ $resident->penduduk->no_reg }}</td>
-                                <td>
-                                    <a class="flex-start"
-                                        href="{{ route('admin.data-penduduk.edit', ['resident' => $resident['id_penduduk']]) }}">
-                                        <svg width="26" height="26" viewBox="0 0 26 26" fill="none"
-                                            xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M13 7.75V14.5" stroke="#225157" stroke-width="2" stroke-linecap="round" />
-                                            <circle cx="13" cy="17.875" r="1" fill="#51526C" stroke="#225157"
-                                                stroke-width="0.25" />
-                                            <path
-                                                d="M24.0246 13.0001C24.0246 6.91116 19.0885 1.9751 12.9996 1.9751C6.91067 1.9751 1.97461 6.91116 1.97461 13.0001C1.97461 19.089 6.91067 24.0251 12.9996 24.0251C19.0885 24.0251 24.0246 19.089 24.0246 13.0001Z"
-                                                stroke="#225157" stroke-width="2" stroke-miterlimit="10" />
-                                        </svg>
-                                    </a>
-                                </td>
-                            </tr>
+                        <tr>
+                            <td class="hidden">{{ $resident->id_penduduk }}</td>
+                            <td>{{ $resident->penduduk->nama }}</td>
+                            <td>{{ $resident->penduduk->nik }}</td>
+                            <td>{{ $resident->penduduk->tgl_lahir }}</td>
+                            <td>{{ $resident->penduduk->no_reg }}</td>
+                            <td>
+                                <a class="flex-start"
+                                    href="{{ route('admin.data-penduduk.edit', ['resident' => $resident['id_penduduk']]) }}">
+                                    <svg width="26" height="26" viewBox="0 0 26 26" fill="none"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M13 7.75V14.5" stroke="#225157" stroke-width="2" stroke-linecap="round" />
+                                        <circle cx="13" cy="17.875" r="1" fill="#51526C" stroke="#225157"
+                                            stroke-width="0.25" />
+                                        <path
+                                            d="M24.0246 13.0001C24.0246 6.91116 19.0885 1.9751 12.9996 1.9751C6.91067 1.9751 1.97461 6.91116 1.97461 13.0001C1.97461 19.089 6.91067 24.0251 12.9996 24.0251C19.0885 24.0251 24.0246 19.089 24.0246 13.0001Z"
+                                            stroke="#225157" stroke-width="2" stroke-miterlimit="10" />
+                                    </svg>
+                                </a>
+                            </td>
+                        </tr>
                     @endforeach
                 </tbody>
             </table>
@@ -124,14 +138,15 @@
                 </thead>
                 <tbody>
                     @foreach ($residents as $resident)
-                            <tr>
-                                <td class="hidden">{{ $resident->id_penduduk }}</td>
-                                <td>{{ $resident->nama }}</td>
-                                <td>{{ $resident->nik }}</td>
-                                <td>{{ $resident->tgl_lahir }}</td>
-                                <td>{{ $resident->no_reg }}</td>
-                                <td class="font-bold {{$resident->status == 'Ditolak' ? 'text-red-600' : 'text-main'}}">{{ $resident->status }}</td>
-                            </tr>
+                        <tr>
+                            <td class="hidden">{{ $resident->id_penduduk }}</td>
+                            <td>{{ $resident->nama }}</td>
+                            <td>{{ $resident->nik }}</td>
+                            <td>{{ $resident->tgl_lahir }}</td>
+                            <td>{{ $resident->no_reg }}</td>
+                            <td class="font-bold {{ $resident->status == 'Ditolak' ? 'text-red-600' : 'text-main' }}">
+                                {{ $resident->status }}</td>
+                        </tr>
                     @endforeach
                 </tbody>
             </table>
