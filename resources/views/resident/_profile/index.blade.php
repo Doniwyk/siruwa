4ed7b26400342e6cae0bdbe1ca93ede52294e42f
doniwyk <doniwyk@gmail.com>
2024-05-25 09:41:01 +0700

    [update] fetch data for resident profile view

--- resources/views/resident/_profile/index.blade.php
+++ resources/views/resident/_profile/index.blade.php
@@ -4,40 +4,22 @@
 <div class="resident-header">{{ $title }}</div>
 <div class="bg-white p-9 rounded-2xl flex space-x-9 mb-6">
     <section class="shrink-0 flex flex-col">
-        <span class="mb-6 text-2xl font-semibold text-secondary">Foto Profil</span>
-        <img src="https://via.placeholder.com/336x258" class="h-[258px] w-[336px] rounded-xl" alt="">
+        <span class="resident-profile-subheader">Foto Profil</span>
+        <img src="{{ $account->urlProfile }}" class="h-[258px] w-[336px] rounded-xl" alt="Profil">
     </section>
     <section class="w-full flex flex-col">
         <span class="mb-6 text-2xl font-semibold text-secondary">Identitas Diri</span>
-        <form action="" class="grid grid-cols-2 grid-flow-row gap-x-9 gap-y-5">
-            <div class="form-group">
-                <label for="nama" class="text-label_light">Nama Lengkap</label>
-                <input type="text" name="" id="" class="form-control" disabled>
-            </div>
-            <div class="form-group">
-                <label for="nama" class="text-label_light">Username</label>
-                <input type="text" name="" id="" class="form-control" disabled>
-            </div>
-            <div class="form-group">
-                <label for="nama" class="text-label_light">Role</label>
-                <input type="text" name="" id="" class="form-control" disabled>
-            </div>
-            <div class="form-group">
-                <label for="nama" class="text-label_light">Email</label>
-                <input type="text" name="" id="" class="form-control" disabled>
-            </div>
-            <div class="form-group">
-                <label for="nama" class="text-label_light">Alamat</label>
-                <input type="text" name="" id="" class="form-control" disabled>
-            </div>
-            <div class="form-group">
-                <label for="nama" class="text-label_light">No. Telepon</label>
-                <input type="text" name="" id="" class="form-control" disabled>
-            </div>
+        <form class="grid grid-cols-2 grid-flow-row gap-x-9 gap-y-5">
+            <x-form.show-input-form :label="'Username'"  :name="'nama'" :value="$account->username" />
+            <x-form.show-input-form :label="'Nama Lengkap'"  :name="'anu'" :value="$detailAccount->nama" />
+            <x-form.show-input-form :label="'Role'"  :name="'reqRegistrasi'" :value="$account->role" />
+            <x-form.show-input-form :label="'Email'"  :name="'email'" :value="$account->email" />
+            <x-form.show-input-form :label="'Alamat Lengkap'"  :name="'reqAlamat'" :value="$detailAccount->alamat" />
+            <x-form.show-input-form :label="'No. Telepon'"  :name="'reqRegistrasi'" :value="$account->noHp" />
         </form>
     </section>
 </div>
 <div class="flex justify-end">
-    <a href="{{ route('resident.profil.edit') }}" class="w-60 bg-secondary text-stone-50 font-semibold py-3 rounded-2xl flex justify-center">Edit Profil</a>
+    <a href="{{ route('resident.profil.edit') }}" class="btn-main">Edit Profil</a>
 </div>
 @endsection
