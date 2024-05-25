1cc67dc4b3c7e718e53b864c71969dd6e4ce5637
doniwyk <doniwyk@gmail.com>
2024-05-25 09:52:06 +0700

    [update] field position

--- resources/views/resident/_profile/index.blade.php
+++ resources/views/resident/_profile/index.blade.php
@@ -10,8 +10,8 @@
     <section class="w-full flex flex-col">
         <span class="mb-6 text-2xl font-semibold text-secondary">Identitas Diri</span>
         <form class="grid grid-cols-2 grid-flow-row gap-x-9 gap-y-5">
-            <x-form.show-input-form :label="'Username'"  :name="'nama'" :value="$account->username" />
             <x-form.show-input-form :label="'Nama Lengkap'"  :name="'anu'" :value="$detailAccount->nama" />
+            <x-form.show-input-form :label="'Username'"  :name="'nama'" :value="$account->username" />
             <x-form.show-input-form :label="'Role'"  :name="'reqRegistrasi'" :value="$account->role" />
             <x-form.show-input-form :label="'Email'"  :name="'email'" :value="$account->email" />
             <x-form.show-input-form :label="'Alamat Lengkap'"  :name="'reqAlamat'" :value="$detailAccount->alamat" />
