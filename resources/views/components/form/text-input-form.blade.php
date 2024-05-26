0d7bcc4863f0c0b5a5329f8596dbbd41936a22e9
doniwyk <doniwyk@gmail.com>
2024-05-26 16:29:03 +0700

    [update] components form

--- resources/views/components/form/text-input-form.blade.php
+++ resources/views/components/form/text-input-form.blade.php
@@ -1,17 +1,28 @@
-<!-- gangerti kenopo type e gak ke pass dadi nulis neh akwokwok, seng iso benerno monggo-->
+<!-- gangerti kenopo type e gak ke pass dadi nulis neh akwokwok, seng iso benerno monggo--> 
 @php
 $type = $type ?? 'text';
 @endphp
 
-<div class="form-group relative" x-data="{ showPassword: false }">
-    <label for="{{ $name }}" class="text-label_light">{{ $label }}</label>
-    <input :type="showPassword ? 'text' : '{{ $type }}'" name="{{ $name }}" id="{{ $name }}" placeholder="{{ $label }}" value="{{ $value ?? '' }}" class="form-control-input">
+<div>
+    <label for="{{ $name }}" class="text-label_light mb-2 block">{{ $label }}</label>
+    <div class="form-group relative" x-data="{ showPassword: false }">
+        <input 
+            :type="showPassword ? 'text' : '{{ $type }}'" 
+            name="{{ $name }}" 
+            id="{{ $name }}" 
+            placeholder="{{ $label }}" 
+            value="{{ $value ?? '' }}" 
+            class="form-control-input {{ $type === 'date' ? 'cursor-pointer' : '' }}" 
+            @if ($type === 'date') onclick="this.showPicker()" @endif
+        >
 
-    @if ($type === 'password')
-        <img src="{{ asset('assets/icons/eye.svg') }}"
-             alt="Show Password"
-             class="absolute top-[42px] right-4 w-5 h-5 cursor-pointer"
-             @click="showPassword = !showPassword"
-             x-bind:src="showPassword ? '{{ asset('assets/icons/eye-slash.svg') }}' : '{{ asset('assets/icons/eye.svg') }}'">
-    @endif
+        @if ($type === 'password')
+            <img 
+                src="{{ asset('assets/icons/eye.svg') }}" 
+                alt="Show Password" 
+                class="right-icon cursor-pointer"
+                @click="showPassword = !showPassword" 
+                x-bind:src="showPassword ? '{{ asset('assets/icons/eye-slash.svg') }}' : '{{ asset('assets/icons/eye.svg') }}'">
+        @endif
+    </div>
 </div>
