78c3e0a9f746d790bfe5bc9a7558cfbb2f9347c6
putricha_ <putrinorchasana@gmail.com>
2024-05-26 13:24:28 +0700

    [add] middleware revalidate

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -22,6 +22,9 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        'revalidate' => \App\Http\Middleware\RevalidateBackHistory::class,
+
+        
     ];
 
     /**
@@ -68,6 +71,7 @@ class Kernel extends HttpKernel
         'isGuest' => \App\Http\Middleware\IsGuest::class,
         'isAuth' => \App\Http\Middleware\IsAuth::class,
         'userAccess' => \App\Http\Middleware\UserAccess::class,
+        'revalidate' => \App\Http\Middleware\RevalidateBackHistory::class,
 
     ];
     protected $commands = [
