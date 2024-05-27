7509600cd9423e4449aed1e267507740c6f059c7
Ihza Nurkhafidh Al-baihaqi <ihza.nk.160903@gmail.com>
2024-05-27 18:08:44 +0700

    addjust [page]: adjust page with design

--- resources/views/admin/_news/create.blade.php
+++ resources/views/admin/_news/create.blade.php
@@ -1,13 +1,45 @@
 @extends('layouts.admin')
 @section('content')
-<div class="header-edit flex-start gap-1">
-    <a href="{{ route('admin.manajemen-berita.index') }}">
-        <x-icon.back />
-    </a>
-    <h1 class="h1-semibold">Verifikasi Dokumen</h1>
-</div>
-<form action="/upload" method="post" enctype="multipart/form-data">
-    <input type="file" name="gambar" accept="image/*">
-    <button type="submit">Upload</button>
-</form>
+    <div class="header-edit flex-start gap-1">
+        <a href="{{ route('admin.manajemen-berita.index') }}">
+            <x-icon.back />
+        </a>
+        <h1 class="h1-semibold">Tulis Berita</h1>
+    </div>
+    <form action="{{ route('admin.manajemen-berita.store') }}" enctype="multipart/form-data" method="POST"
+        class="flex flex-col gap-6 items-end" id="add_news_form">
+        @csrf
+        @method('POST')
+        <section class="grid grid-rows-2 grid-cols-4 gap-6 w-full">
+            <div class="row-span-2 col-span-1 relative " id="file-1">
+                <input type="file" name="image" id="image" class="hidden">
+                <label for="image" id="file-1-preview" class="dropzone absolute w-full h-full flex-center flex-col">
+                    <div >
+                        <span class="font-semibold flex-col flex-center text-main ">
+                            <x-icon.galery />
+                            Upload Gambar
+                        </span>
+                    </div>
+                </label>
+            </div>
+            <div class="form-group col-span-3">
+                <label for="judul" class="text-main font-semibold">Judul Berita</label>
+                <input type="text" id ="judul" name ="judul" class="px-6 py-2 rounded-2xl outline-none"
+                    placeholder="Tuliskan Judul Berita" />
+            </div>
+            <div class="form-group col-span-3">
+                <label for="penulis" class="text-main font-semibold">Penulis</label>
+                <input type="text" id ="penulis" name ="penulis" class="hidden" value="{{ $account->nama }}" />
+                <input type="text" id ="penulis" name ="penulis"
+                    class="px-6 py-2 rounded-2xl outline-none bg-[#DDE9EA] text-main" value="{{ $account->nama }}"
+                    disabled />
+            </div>
+        </section>
+        <section class="flex flex-col gap-3 w-full">
+            <label for="description" class="text-main font-semibold">Isi Artikel</label>
+            <textarea id="editor" name="editor" class="ck-editor__editable ck-editor__editable_inline"></textarea>
+        </section>
+        <button type="submit" id="submit-all" class="w-[15rem] h-[3rem] bg-main text-white rounded-2xl font-semibold">
+            Upload </button>
+    </form>
 @endsection
