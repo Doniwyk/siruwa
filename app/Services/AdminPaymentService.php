02d2017f860f34ac74433176ee60855dbabf9746
Ihza Nurkhafidh Al-baihaqi <ihza.nk.160903@gmail.com>
2024-05-27 17:40:38 +0700

    add [paginating]

--- app/Services/AdminPaymentService.php
+++ app/Services/AdminPaymentService.php
@@ -85,14 +85,14 @@ class AdminPaymentService implements AdminPaymentContract
 
     public function getValidatedPayment()
     {
-        $validatedPayments = PaymentModel::where('status', 'Terverifikasi')->with('penduduk', 'admin')->get();
+        $validatedPayments = PaymentModel::where('status', ['Terverifikasi', 'Ditolak'])->with('penduduk', 'admin')->paginate(10);
 
         return $validatedPayments;
     }
 
     public function getSubmission()
     {
-        $getSubmission = PaymentModel::where('status', 'Belum Terverifikasi')->with('penduduk', 'admin')->get();
+        $getSubmission = PaymentModel::where('status', 'Belum Terverifikasi')->with('penduduk', 'admin')->paginate(10);
 
         $getDeathFundAmount = DeathFundModel::where('status', 'Lunas')
         ->count() * 10000;
