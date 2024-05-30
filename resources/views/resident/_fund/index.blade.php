c3e594191d127ace67e4ecc48e454fb72f9ccfb4
doniwyk <doniwyk@gmail.com>
2024-05-30 12:35:37 +0700

    feat: nominal input is multiply of 10.000

--- resources/views/resident/_fund/index.blade.php
+++ resources/views/resident/_fund/index.blade.php
@@ -93,12 +93,12 @@
                 </div>
                 <div class="whitespace-nowrap flex items-center">
                     <div class="relative w-[180px] mr-9">
-                        <select name="" id="" class="resident-select">
+                        <select name="" id="" class="resident-select cursor-pointer">
                             <option value="">Filter Data</option>
                             <option value="P">Anu</option>
                         </select>
-                        <img src="{{ asset('assets/icons/filter.svg') }}" alt="Filter Icon" class="left-icon">
-                        <img src="{{ asset('assets/icons/arrow.svg') }}" alt="Arrow Icon" class="right-icon">
+                        <img src="{{ asset('assets/icons/filter.svg') }}" alt="Filter Icon" class="left-icon pointer-events-none">
+                        <img src="{{ asset('assets/icons/arrow.svg') }}" alt="Arrow Icon" class="right-icon pointer-events-none">
                     </div>
                     <button class="button-pay" @click="showModal = true">Lakukan Pembayaran</button>
                 </div>
@@ -145,28 +145,29 @@
                     <div class="flex flex-col mb-4">
                         <label class="text-secondary text-xl font-semibold mb-2">Jenis Iuran</label>
                         <div class="relative">
-                            <select class="resident-input">
+                            <select class="resident-input cursor-pointer">
                                 <option value="">Pilih Jenis Pembayaran</option>
                                 <option value="sampah">Iuran Sampah</option>
                                 <option value="kematian">Iuran Kematian</option>
                             </select>
-                            <img src="{{ asset('assets/icons/arrow.svg') }}" alt="Dropdown Icon" class="right-icon">
+                            <img src="{{ asset('assets/icons/arrow.svg') }}" alt="Dropdown Icon" class="right-icon pointer-events-none">
                         </div>
                     </div>
                     <div class="flex flex-col mb-4">
                         <label class="text-secondary text-xl font-semibold mb-2">Metode Pembayaran</label>
                         <div class="relative">
-                            <select class="resident-input">
+                            <select class="resident-input cursor-pointer">
                                 <option value="">Pilih Metode</option>
                                 <option value="tunai">Tunai</option>
                                 <option value="transfer">Transfer</option>
                             </select>
-                            <img src="{{ asset('assets/icons/arrow.svg') }}" alt="Dropdown Icon" class="right-icon">
+                            <img src="{{ asset('assets/icons/arrow.svg') }}" alt="Dropdown Icon" class="right-icon pointer-events-none">
                         </div>
                     </div>
                     <div class="flex flex-col mb-9">
                         <label class="text-secondary text-xl font-semibold mb-2">Nominal</label>
-                        <input type="text" id="nominal" name="nominal" class="resident-input" inputmode="numeric" pattern="[0-9]*" placeholder="Masukkan Nominal" oninput="formatNumber(this)">
+                        <input type="text" id="nominal" name="nominal" class="resident-input" inputmode="numeric" pattern="[0-9]*" placeholder="Masukkan Nominal" oninput="formatNumber(this); validateMultipleOfTenThousand(this);">
+                        <p id="nominal-error" class="text-red-500 text-sm mt-1 hidden">Nominal harus kelipatan 10,000.</p>
                     </div>
                     <div class="flex items-center justify-center w-full">
                         <div id="dropzone" class="relative flex flex-col items-center justify-center w-full h-64 border-secondary border-2 border-dashed rounded-2xl cursor-pointer hover:bg-gray-100">
@@ -177,59 +178,69 @@
                             <input id="dropzone-file" type="file" class="hidden" accept="image/*">
                             <img id="preview-image" class="hidden absolute inset-0 object-cover w-full h-full rounded-2xl" alt="Uploaded Image">
                         </div>
-                    </div>
-                    
-                    <script>
-                        const dropzone = document.getElementById('dropzone');
-                        const dropzoneFileInput = document.getElementById('dropzone-file');
-                        const previewImage = document.getElementById('preview-image');
-
-                        ['dragover', 'dragenter'].forEach(event => {
-                            dropzone.addEventListener(event, function(e) {
-                                e.preventDefault();
-                                dropzone.classList.add('bg-gray-100');
-                            });
-                        });
-
-                        ['dragleave', 'drop'].forEach(event => {
-                            dropzone.addEventListener(event, function(e) {
-                                e.preventDefault();
-                                dropzone.classList.remove('bg-gray-100');
-                                if (e.type === 'drop') {
-                                    dropzoneFileInput.files = e.dataTransfer.files;
-                                    displayPreview(dropzoneFileInput.files[0]);
-                                }
-                            });
-                        });
-
-                        dropzone.addEventListener('click', () => dropzoneFileInput.click());
-
-                        dropzoneFileInput.addEventListener('change', function(e) {
-                            displayPreview(e.target.files[0]);
-                        });
-
-                        function displayPreview(file) {
-                            const reader = new FileReader();
-                            reader.onload = () => {
-                                previewImage.src = reader.result;
-                                previewImage.classList.remove('hidden');
-                            };
-                            reader.readAsDataURL(file);
-                        }
-
-                        function formatNumber(input) {
-                            let value = input.value.replace(/\D/g, ''); // Remove non-digits
-                            let formatted = value.replace(/\B(?=(\d{3})+(?!\d))/g, '.'); // Add dot as thousand separator
-                            input.value = formatted;
-                        }
-                    </script>                    
+                    </div>                  
                     
                     <div class="mt-9 flex space-x-5">
-                        <button class="py-2 font-semibold text-secondary rounded-2xl w-1/2 border-2 border-secondary" @click.prevent="showModal = false">Batal</button>
-                        <button class="py-2 bg-secondary font-semibold text-white rounded-2xl w-1/2">Konfirmasi</button>
+                        <button class="btn-secondary" @click.prevent="showModal = false">Batal</button>
+                        <button class="btn-main">Konfirmasi</button>
                     </div>
                 </form>
             </div>
         </div>
     </div>
+
+    <script>
+        const dropzone = document.getElementById('dropzone');
+        const dropzoneFileInput = document.getElementById('dropzone-file');
+        const previewImage = document.getElementById('preview-image');
+
+        ['dragover', 'dragenter'].forEach(event => {
+            dropzone.addEventListener(event, function(e) {
+                e.preventDefault();
+                dropzone.classList.add('bg-gray-100');
+            });
+        });
+
+        ['dragleave', 'drop'].forEach(event => {
+            dropzone.addEventListener(event, function(e) {
+                e.preventDefault();
+                dropzone.classList.remove('bg-gray-100');
+                if (e.type === 'drop') {
+                    dropzoneFileInput.files = e.dataTransfer.files;
+                    displayPreview(dropzoneFileInput.files[0]);
+                }
+            });
+        });
+
+        dropzone.addEventListener('click', () => dropzoneFileInput.click());
+
+        dropzoneFileInput.addEventListener('change', function(e) {
+            displayPreview(e.target.files[0]);
+        });
+
+        function displayPreview(file) {
+            const reader = new FileReader();
+            reader.onload = () => {
+                previewImage.src = reader.result;
+                previewImage.classList.remove('hidden');
+            };
+            reader.readAsDataURL(file);
+        }
+
+        function formatNumber(input) {
+            let value = input.value.replace(/\D/g, ''); 
+            let formatted = value.replace(/\B(?=(\d{3})+(?!\d))/g, '.'); 
+            input.value = formatted;
+        }
+
+        function validateMultipleOfTenThousand(input) {
+            const errorElement = document.getElementById('nominal-error');
+            let value = input.value.replace(/\D/g, ''); 
+            if (value % 10000 !== 0) {
+                errorElement.classList.remove('hidden');
+            } else {
+                errorElement.classList.add('hidden');
+            }
+        }
+    </script>  
 @endsection
