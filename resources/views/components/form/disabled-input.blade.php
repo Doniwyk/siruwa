091e56eaa737acfe9db93e38cb02c227f1d1eef8
Ihza Nurkhafidh Al-baihaqi <ihza.nk.160903@gmail.com>
2024-06-01 17:38:45 +0700

    add [format]: add numbering format for passed data

--- resources/views/components/form/disabled-input.blade.php
+++ resources/views/components/form/disabled-input.blade.php
@@ -1,17 +1,14 @@
 <div class="form-group">
-    {{$prevValue}}
     <label for="nama" class="text-label_light">{{ $label }}</label>
     <input type="text" name="{{ $prevName }}" id="{{ $prevName }}" class="form-control" disabled
         placeholder="{{ $label }}" 
         value=
-        @if ($label == 'No. Registrasi')
-            "{{ $prevValue }}"
+        @if (is_bool($prevValue))
+            "{{$prevValue ? 'Iya' : 'Tidak'}}"
+        @elseif(is_double($prevValue))
+            "{{ number_format($prevValue, 0, ',', '.') }}"
         @else
-            @if (is_string($prevValue))    
-                "{{ $prevValue }}"
-            @else
-                "{{ $prevValue == 1 ? 'Iya' : 'Tidak' }}"
-            @endif
+            "{{$prevValue}}"
         @endif
         >
 
@@ -19,11 +16,14 @@
         <input type="text" name="{{ $reqName }}" id="{{ $reqName }}" class="form-control border-2 border-input-border" disabled
             placeholder="{{ $label }}" 
             value=
-            @if (is_string($reqValue))
-                "{{ $reqValue }}"
+            @if (is_bool($reqValue))
+                "{{$reqValue ? 'Iya' : 'Tidak'}}"
+            @elseif(is_double($reqValue))
+                "{{ number_format($reqValue, 0, ',', '.') }}"
             @else
-                "{{ $reqValue == 1 ? 'Iya' : 'Tidak' }}"
-            @endif>
+                "{{$reqValue}}"
+            @endif
+            >
     @endif
 </div>
 
