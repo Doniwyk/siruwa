765f9ac57721cde6b974a50ae2735e4f3e650a43
Ihza Nurkhafidh Al-baihaqi <ihza.nk.160903@gmail.com>
2024-06-01 11:30:36 +0700

    make [responsive]: make responsive component

--- resources/views/components/add-news-button.blade.php
+++ resources/views/components/add-news-button.blade.php
@@ -1,6 +1,6 @@
-<div class="add-news flex justify-end hover:scale-95 transition-transform">
+<div class="add-news flex justify-end button-hover">
     <a href="{{ $route }}">
-    <button class="h-[3rem] w-[15rem] bg-main text-white rounded-2xl flex-center gap-3">
+    <button class="h-[3rem] sm:w-[3rem] lg:w-[15rem] bg-main text-white  rounded-2xl flex-center gap-3">
         <svg width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path
                 d="M10.4993 18.3332C15.0827 18.3332 18.8327 14.5832 18.8327 9.99984C18.8327 5.4165 15.0827 1.6665 10.4993 1.6665C5.91602 1.6665 2.16602 5.4165 2.16602 9.99984C2.16602 14.5832 5.91602 18.3332 10.4993 18.3332Z"
@@ -10,7 +10,7 @@
             <path d="M10.5 13.3327V6.66602" stroke="white" stroke-width="2" stroke-linecap="round"
                 stroke-linejoin="round" />
         </svg>
-        <label for="">Buat Berita</label>
+        <label for="" class="sm:hidden lg:block">Buat Berita</label>
     </button>
     </a>
 </div>
