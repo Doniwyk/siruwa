a7f9d60bcad807e1cf4671002a5e30f20bf4b5f5
Ihza Nurkhafidh Al-baihaqi <ihza.nk.160903@gmail.com>
2024-06-01 11:29:50 +0700

    make [responsive]: make responsive admin view | add [modal]: add modal to showing popup |

--- resources/views/admin/_fund/index.blade.php
+++ resources/views/admin/_fund/index.blade.php
@@ -1,7 +1,44 @@
 @extends('layouts.admin')
+@section('modal')
+    <div class="absolute min-h-full min-w-full flex-center bg-black/50 hidden z-50" id="payment-modal_parent">
+        <div class="p-10 rounded-2xl bg-white center z-50 overflow-hidden" id="payment-modal">
+            <form class="flex flex-col gap-9" method="POST">
+                @csrf
+                @method('PUT')
+                <input type="text" class="hidden" value="{{$adminId}}" name="id_admin">
+                <h1 class="text-xl font-bold text-main text-center">Konfirmasi Pembayaran</h1>
+                <div id="image-preview" class="h-[11.75rem] flex-center">
+                    <img src="" alt="Bukti Pembayaran" class="h-full bg-white">
+                </div>
+                <fieldset>
+                    <div class="flex gap-4 justify-between items-center">
+                        <label for="metode_display bg-main">Metode Pembayaran</label>
+                        <input type="text" name="metode" id="metode" value="" class="hidden">
+                        <input type="text" name="metode_display" id="metode_display"
+                            class="w-[17.25rem] py-2 px-4 outline-none rounded-2xl border-2 border-outline" value=""
+                            disabled>
+                    </div>
+                    <div class="flex gap-4 justify-between mt-4 items-center">
+                        <label for="jumlah_display ">Konfirmasi Nominal</label>
+                        <input type="text" name="jumlah" id="jumlah" value="" class="hidden">
+                        <input type="text" name="jumlah_display" id="jumlah_display"
+                            class="w-[17.25rem] py-2 px-4 outline-none rounded-2xl border-2 border-outline" value=""
+                            disabled>
+                    </div>
+                </fieldset>
+                <div class="wrapper flex gap-5">
+                    <button class="mt-5 bg-red-600 text-white px-4 py-2 rounded-3xl flex-1" type="submit" name="action"
+                        value="tolak"> Tolak</button>
+                    <button type="submit" class="mt-5 bg-main text-white px-4 py-2 rounded-3xl flex-1" name="action"
+                        value="terima"> Konfirmasi</button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
 @section('content')
     <h1 class="h1-semibold">Data Penduduk</h1>
-    <div class="summary-card_fund">
+    <div class="summary-card_fund ">
         <x-card :label="'Dana Kematian'" :value="$fundData['deathFundTotal']" />
         <x-card :label="'Dana Sampah'" :value="$fundData['garbageFundTotal']" />
         <x-card :label="'Tunggakan'" :type="'danger'" :value="$fundData['tunggakan']" />
@@ -27,8 +64,8 @@
                 <thead>
                     <tr>
                         <th>Nomor KK</th>
-                        <th>Tgl Permintaan</th>
-                        <th>No. Telepon</th>
+                        <th class="sm:hidden lg:table-cell">Tgl Permintaan</th>
+                        <th >No. Telepon</th>
                         <th>Detail</th>
                     </tr>
                 </thead>
@@ -39,17 +76,13 @@
                     @foreach ($fundData['getSubmission'] as $data)
                         <tr>
                             <td>{{ $data->nomor_kk }}</td>
-                            <td>{{ $data->created_at }}</td>
+                            <td class="sm:hidden lg:table-cell">{{ $data->created_at }}</td>
                             <td>{{ $data->admin->noHp }}</td>
                             <td>
-                                <button class="w-[25px] h-[25px] flex-center"
-                                    onclick="showPopup('#payment-card-{{ $i }}', '#modal-parent-{{ $i }}')">
+                                <button class="w-[25px] h-[25px] flex-center" id="button-{{ $i }}"
+                                    onclick="getDataPembayaran({{ $data->id_pembayaran }})">
                                     <x-icon.detail />
                                 </button>
-                            </td>
-                            <td class="w-full h-full flex-center bg-black/50 absolute top-0 left-0 hidden"
-                                id="modal-parent-{{ $i }}">
-                                <x-shared.confirmation-payment-card :formId="$i" :fundData="$data" />
                             </td>
                         </tr>
                         @php
@@ -68,7 +101,7 @@
                 <thead>
                     <tr>
                         <th>Nomor KK</th>
-                        <th>Tgl Permintaan</th>
+                        <th class="sm:hidden lg:table-cell">Tgl Permintaan</th>
                         <th>No. Telepon</th>
                         <th>Detail</th>
                     </tr>
@@ -80,7 +113,7 @@
                     @foreach ($history as $data)
                         <tr>
                             <td>{{ $data->nomor_kk }}</td>
-                            <td>{{ $data->created_at }}</td>
+                            <td class="sm:hidden lg:table-cell">{{ $data->created_at }}</td>
                             <td>{{ $data->admin->noHp }}</td>
                             <td class="font-semibold {{ $data->status == 'Ditolak' ? 'text-red-600' : 'text-main' }}">
                                 {{ $data->status }}</td>
