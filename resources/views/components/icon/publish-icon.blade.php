7e3d4fe8b4051bb1c20909a91416874e18351a25
zalaaaaa <ihza.nk.160903@gmail.com>
2024-06-06 14:03:28 +0700

    add [icon]

---
+++ resources/views/components/icon/publish-icon.blade.php
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- Generator: Adobe Illustrator 28.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
+<style type="text/css">
+	.st0{fill:none;stroke:#000000;stroke-width:36;stroke-miterlimit:10;}
+</style>
+<path class="st0" d="M256,24.8C128.3,24.8,24.8,128.3,24.8,256S128.3,487.2,256,487.2S487.2,383.7,487.2,256S383.7,24.8,256,24.8z"
+	/>
+<circle cx="256" cy="160" r="32"/>
+<path d="M320,360c0,13.3-10.7,24-24,24h-80c-13.3,0-24-10.7-24-24s10.7-24,24-24h24v-64h-24c-13.3,0-24-10.7-24-24s10.7-24,24-24h48
+	c13.3,0,24,10.7,24,24v88h8C309.3,336,320,346.7,320,360z"/>
+</svg>
