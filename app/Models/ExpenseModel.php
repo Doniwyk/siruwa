7ece69b225651c8ba4ba19eaa6ac2c05a75abfbc
putricha_ <putrinorchasana@gmail.com>
2024-06-06 23:55:55 +0700

    [add] models for income and expense table

---
+++ app/Models/ExpenseModel.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ExpenseModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'pengeluaran';
+    protected $primaryKey = 'id_pengeluaran';
+
+    protected $fillable = ['jumlah_pengeluaran','jenis_pengeluaran'];
+}
