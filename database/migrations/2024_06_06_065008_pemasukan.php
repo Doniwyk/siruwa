1df5130ca905756dc5874f17d69b3a6b654d0180
putricha_ <putrinorchasana@gmail.com>
2024-06-06 23:54:56 +0700

    [add] database for financial report

---
+++ database/migrations/2024_06_06_065008_pemasukan.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pemasukan', function (Blueprint $table) {
+            $table->id('id_pemasukan');
+            $table->double('jumlah_pemasukan');
+            $table->enum('jenis_pemasukan',['Iuran Kematian','Iuran Sampah']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pemasukan');
+    }
+};
