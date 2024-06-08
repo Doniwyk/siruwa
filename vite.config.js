db91ceebd55b67de665f626fa90e17e14ba9666b
doniwyk <doniwyk@gmail.com>
2024-06-08 11:48:27 +0700

    feat: coba hosting vite v2

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,14 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+        rollupOptions: {
+            input: {
+                app: 'resources/js/app.js',
+                style: 'resources/css/app.css'
+            }
+        }
+    }
 });
