7d766873fbbc4db911821aad2772b09cc95ba233
zalaaaaa <ihza.nk.160903@gmail.com>
2024-06-08 10:25:39 +0700

    rename [name]: renaming name of class with pascal case

--- app/View/Components/Icon/comment.php
+++ app/View/Components/Icon/comment.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class comment extends Component
+class Comment extends Component
 {
     /**
      * Create a new component instance.
--- app/View/Components/Icon/home.php
+++ app/View/Components/Icon/home.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class home extends Component
+class Home extends Component
 {
     /**
      * Create a new component instance.
