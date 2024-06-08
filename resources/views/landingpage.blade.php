9d45311002e41540e378ead4751c58d2309d4edc
doniwyk <doniwyk@gmail.com>
2024-06-08 12:00:39 +0700

    feat: coba host asu v3

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -5,11 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    {{-- <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/output.css') }}"> --}}
-    @vite('resources/css/app.css')
-    @vite('resources/css/output.css')
-    <title>SIRUWA</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/output.css') }}">
+    {{-- @vite('resources/css/app.css')
+    @vite('resources/css/output.css') --}}
+    <title>ASU</title>
 
 
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
