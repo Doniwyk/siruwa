a3f71ecbd938074d084c53f7bf54605e66573e6f
zalaaaaa <ihza.nk.160903@gmail.com>
2024-06-09 10:15:20 +0700

    add [meta]: add meta viewport in admin layout v2

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://cdn.jsdelivr.net/npm/luxon@2.1.0/build/global/luxon.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
