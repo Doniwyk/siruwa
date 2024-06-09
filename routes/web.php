e046c450cfb2e5c70d78007f0e67c93035c59760
putricha_ <putrinorchasana@gmail.com>
2024-06-09 14:43:16 +0700

    [add] middleware for landing page

--- routes/web.php
+++ routes/web.php
@@ -54,7 +54,7 @@ Route::get('/logout', [AuthenticationController::class, 'doLogout'])->middleware
 
 //==================================ROUTE LANDING PAGE========================================
 
-Route::get('/', [DashboardController::class, 'indexLandingPage'])->name('index');
+Route::get('/', [DashboardController::class, 'indexLandingPage'])->name('index')->middleware('redirectIfAdmin');
 Route::get('/fetch-events', [DashboardController::class, 'fetchEvents']);
 //==================================ROUTE STATISTIC FOR ADMIN========================================
 
