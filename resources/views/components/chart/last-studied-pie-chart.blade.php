dcac1a7a184ae0ba5889231bc1d344d48f0b4e6a
zalaaaaa <ihza.nk.160903@gmail.com>
2024-06-11 09:50:58 +0700

    add [skeleton]: add skeleton before chart ready to display

--- resources/views/components/chart/last-studied-pie-chart.blade.php
+++ resources/views/components/chart/last-studied-pie-chart.blade.php
@@ -1,6 +1,20 @@
 <div class="flex-center relative flex-col">
     <h3 class="text-xl font-semibold text-main top-0">Rasio Pendidikan Terakhir Penduduk</h3>
-    <canvas id="pendidikan_terakhir" class="md:max-w-full"></canvas>
+    <div role="status" id="pendidikan_terakhir_skeleton"
+        class="w-full p-4 border border-gray-200 rounded shadow animate-pulse md:p-6 dark:border-gray-700">
+        <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32 mb-2.5"></div>
+        <div class="w-48 h-2 mb-10 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+        <div class="flex items-baseline mt-4">
+            <div class="w-full bg-gray-200 rounded-t-lg h-72 dark:bg-gray-700"></div>
+            <div class="w-full h-56 ms-6 bg-gray-200 rounded-t-lg dark:bg-gray-700"></div>
+            <div class="w-full bg-gray-200 rounded-t-lg h-72 ms-6 dark:bg-gray-700"></div>
+            <div class="w-full h-64 ms-6 bg-gray-200 rounded-t-lg dark:bg-gray-700"></div>
+            <div class="w-full bg-gray-200 rounded-t-lg h-80 ms-6 dark:bg-gray-700"></div>
+            <div class="w-full bg-gray-200 rounded-t-lg h-72 ms-6 dark:bg-gray-700"></div>
+            <div class="w-full bg-gray-200 rounded-t-lg h-80 ms-6 dark:bg-gray-700"></div>
+        </div>
+    </div>
+    <canvas id="pendidikan_terakhir" class="max-w-full"></canvas>
 </div>
 <script>
     const JOB_INITIAL_DATA = {
@@ -80,8 +94,20 @@
             }
         };
         const myChart3 = new Chart(ctx3, config3);
+        return new Promise((resolve, reject) => {
+            if (myChart3) {
+                resolve(true)
+            }
+        });
 
     }
 
-    createLastStudiedChart();
+    createLastStudiedChart()
+        .then(() => {
+            const skeleton = document.querySelector('#pendidikan_terakhir_skeleton')
+            const chart = document.querySelector('#pendidikan_terakhir');
+
+            skeleton.classList.add('hidden')
+            chart.classList.remove('hidden')
+        });;
 </script>
